1bf5cdb91d0ed01708d4edbf789eefa97e342413
Ladrillo <gabriel@cabrejas.net>
2022-03-28 19:53:06 +0200

    committing to "lecture"

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -88,7 +88,7 @@ test('Can submit a new todo, and shows up uncompleted', async () => {
 
   fireEvent.change(input, { target: { value: 'we are typing this' } })
   screen.getByDisplayValue('we are typing this')
-  expect(input).toHaveValue('')
+  expect(input).toHaveValue('we are typing this')
 
   fireEvent.click(submit)
   await screen.findByText('we are typing this')
