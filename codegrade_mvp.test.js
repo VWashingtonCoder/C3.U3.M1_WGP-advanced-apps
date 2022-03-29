33f667e163a6e9f5fa6726d12ac2f36853bb2e2e
Vincent Washington <97264834+VWashingtonCoder@users.noreply.github.com>
2022-03-29 13:01:17 -0600

    Update codegrade_mvp.test.js
    Added Class Notes

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -1,9 +1,23 @@
+// mock server imports
 import server from './backend/mock-server'
-import React from 'react'
-import App from './frontend/components/App'
-import { render, screen, fireEvent } from '@testing-library/react'
 import { resetTodos } from './backend/helpers'
+// we are going use JSX so we need React
+import React from 'react'
+// components to test
+import App from './frontend/components/App'
+import Todo from './frontend/components/Todo'
+// imports from the testing libraries
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+
+// we can mock ANY module
+jest.mock('uuid', () => {
+  return {
+    v4() {
+      return 'xyz'
+    }
+  }
+})
 
 // not needed for module project, START
 beforeAll(() => { server.listen() })
@@ -12,11 +26,87 @@ afterEach(() => { server.resetHandlers() })
 beforeEach(() => { resetTodos() })
 // not needed for module project, END
 
-test('App is NOT a class-based component', async () => {
+test('App renders without crashing', async () => {
   render(<App />)
-  await screen.findByText('laundry')
-  expect(
-    App.prototype &&
-    App.prototype.isReactComponent
-  ).not.toBeTruthy()
+  // findyText will retry for a while and fail the test if not found
+  await screen.findByText('laundry', { exact: false }) // only findEtc works with async/await
+  // screen.debug()
+})
+test('Todo component renders uncompleted todos correctly', () => {
+  render(<Todo
+    todo={{ id: 'xyz', name: 'doit', completed: false }}
+    toggleStatus={Function.prototype}
+  />)
+  const todo = screen.queryByText('doit', { exact: false })
+
+  // NOOOOO! WE SHOULD ONLY QUERY BY THINGS THE USER CAN READ
+  const todoAnotherWay = document.querySelector('.todo')
+
+  expect(todo).toBeInTheDocument()
+  expect(todo).toBeVisible()
+
+  // sometimes we need to check that something is NOT there
+  // the queryBy queries put a null value in the variable if the node is not there
+  const todoNotThere = screen.queryByText('not here', { exact: false })
+  expect(todoNotThere).toBe(null)
+  expect(todoNotThere).not.toBeInTheDocument()
+})
+test('Todo component renders completed todos correctly', () => {
+  render(
+    <Todo
+      todo={{ id: 'abc', name: 'check test', completed: true }}
+      toggleStatus={Function.prototype}
+    />
+  )
+  // if you want to make the search text more lenient (capitalization, length...)
+  const todoCheck = screen.queryByText('✔️', { exact: false })
+  expect(todoCheck).toBeInTheDocument() // assertion
+  expect(todoCheck).toBeVisible() // another assertion
+
+  // getBy selectors FAIL THE TEST IMMEDIATELY if the node is not there
+  // screen.getByText('foobar', { exact: false })
+})
+test('Renders completeds and uncompleteds correct', () => {
+  const { rerender } = render(<Todo
+    todo={{ id: 'xyz', name: 'doit', completed: false }}
+    toggleStatus={Function.prototype}
+  />)
+  screen.getByText(/doit/, { exact: false })
+  rerender(<Todo
+    todo={{ id: 'xyz', name: 'Do It Now', completed: true }}
+    toggleStatus={Function.prototype}
+  />)
+  screen.getByText(/do it now/i)
+})
+test('Todos can be completed by the user', async () => {
+  render(<App />)
+  const laundry = await screen.findByText('laundry', { exact: false })
+  fireEvent.click(laundry)
+  await screen.findByText('laundry ✔️', { exact: false })
+})
+test('Can submit a new todo, and shows up uncompleted', async () => {
+  render(<App />)
+  await screen.findByText('laundry', { exact: false })
+  // capturing the nodes of interest
+  const input = screen.getByPlaceholderText('Type todo')
+  // these are to be considered escape hatches for things that are hard to select
+  const submit = screen.getByTestId('theSubmit')
+  const submitCheating = document.querySelector('input[type=submit]')
+
+  fireEvent.change(input, { target: { value: 'we are typing this' } })
+  screen.getByDisplayValue('we are typing this')
+  expect(input).toHaveValue('we are typing this')
+
+  fireEvent.click(submit)
+  await screen.findByText('we are typing this', { exact: false })
+  expect(input).toHaveValue('')
+})
+test('click handler gets clickes', () => {
+  const spy = jest.fn()
+  render(<Todo
+    todo={{ id: 'xyz', name: 'doit', completed: false }}
+    toggleStatus={spy}
+  />)
+  fireEvent.click(screen.getByText('doit', { exact: false }))
+  expect(spy).toHaveBeenCalled()
 })
